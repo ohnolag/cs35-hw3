dbbc70da526c23110a36cf63910d71ae9617076f
Omar Elamri <ea_do@icloud.com>
2025-04-22 12:50:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Assignment 3 Starter Code
+
+Please refer to the directions available on the [course webpage](https://cs35l.org/assign/assign3).
+
+# Action Items
+Make sure you've followed the Setup instructions before continuing. (Installing Chrome, Node.js, and Python 3.11 and above + running `./helper init`).
+
+- [ ] Complete the [Tic-Tac-Toe React tutorial](https://react.dev/learn/tutorial-tic-tac-toe) while writing down notes in `tic-tac-toe.txt`. All changes should be made in `src/App.jsx`. Use `./helper dev` to view your changes.
+- [ ] Change Tic-Tac-Toe to conform with the Chorus Lapilli rules while writing down notes in `chorus-lapilli.txt`. Use `./helper dev` to view your changes.
+- [ ] Write test cases in `test_chorus_lapilli.py` to make sure your solution works. Run these tests using `./helper test`.
+- [ ] Create a ZIP file for submission to Gradescope. Running `./helper zip` will create a ZIP file called `assign.zip`.
---
+++ test_chorus_lapilli.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+import unittest
+import os
+import sys
+import argparse
+import subprocess
+import unittest
+import urllib.request
+
+
+class TestChorusLapilli(unittest.TestCase):
+    '''Integration testing for Chorus Lapilli
+
+    This class handles the entire react start up, testing, and take down
+    process. Feel free to modify it to suit your needs.
+    '''
+
+    # ========================== [USEFUL CONSTANTS] ===========================
+
+    # Vite default startup address
+    VITE_HOST_ADDR = 'http://localhost:5173'
+
+    # XPATH query used to find Chorus Lapilli board tiles
+    BOARD_TILE_XPATH = '//button[contains(@class, \'square\')]'
+
+    # Sets of symbol classes - each string contains all valid characters
+    # for that particular symbol
+    SYMBOL_BLANK = ''
+    SYMBOL_X = 'Xx'
+    SYMBOL_O = '0Oo'
+
+    # ======================== [SETUP/TEARDOWN HOOKS] =========================
+
+    @classmethod
+    def setUpClass(cls):
+        '''This function runs before testing occurs.
+
+        Bring up the web app and configure Selenium
+        '''
+
+        env = dict(os.environ)
+        env.update({
+            # Prevent React from starting its own browser window
+            'BROWSER': 'none',
+        })
+
+        subprocess.run(['npm', 'install'],
+                           stdout=subprocess.DEVNULL,
+                           stderr=subprocess.DEVNULL,
+                           env=env,
+                           check=True)
+
+        # Await Webserver Start
+        cls.vite = subprocess.Popen(
+            ['npm', 'run', 'dev'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            env=env)
+
+        if cls.vite.stdout is None:
+            raise OSError("Vite failed to start")
+        for _ in cls.vite.stdout:
+            try:
+                with urllib.request.urlopen(cls.VITE_HOST_ADDR):
+                    break
+
+            except IOError:
+                pass
+
+            # Ensure Vite does not terminate early
+            if cls.vite.poll() is not None:
+                raise OSError('Vite terminated before test')
+        if cls.vite.poll() is not None:
+            raise OSError('Vite terminated before test')
+
+        cls.driver = Browser()
+        cls.driver.get(cls.VITE_HOST_ADDR)
+        cls.driver.implicitly_wait(0.5)
+
+    @classmethod
+    def tearDownClass(cls):
+        '''This function runs after all testing have run.
+
+        Terminate Vite and take down the Selenium webdriver.
+        '''
+        cls.vite.terminate()
+        cls.vite.wait()
+        cls.driver.quit()
+
+    def setUp(self):
+        '''This function runs before every test.
+
+        Refresh the browser so we get a new board.
+        '''
+        self.driver.refresh()
+
+    def tearDown(self):
+        '''This function runs after every test.
+
+        Not needed, but feel free to add stuff here.
+        '''
+
+    # ========================== [HELPER FUNCTIONS] ===========================
+
+    def assertBoardEmpty(self, tiles):
+        '''Checks if all board tiles are empty.
+
+        Arguments:
+          tiles: List[WebElement] - a board consisting of 9 buttons elements
+        '''
+        if len(tiles) != 9:
+            raise AssertionError('tiles is not a 3x3 grid')
+        for i, tile in enumerate(tiles):
+            if tile.text.strip():
+                raise AssertionError(f'tile {i} is not empty: '
+                                     f'\'{tile.text}\'')
+
+    def assertTileIs(self, tile, symbol_set):
+        '''Checks if a certain tile has a certain symbol.
+
+        Arguments:
+          tile: WebElement - the button element to check
+          symbol_set: str - a string containing all the valid symbols
+        Raises:
+          AssertionError - if tile is not in the symbol set
+        '''
+        if symbol_set is None:
+            return
+        if symbol_set == self.SYMBOL_BLANK:
+            name = 'BLANK'
+        elif symbol_set == self.SYMBOL_X:
+            name = 'X'
+        elif symbol_set == self.SYMBOL_O:
+            name = 'O'
+        else:
+            name = 'in symbol_set'
+        text = tile.text.strip()
+        if ((symbol_set == self.SYMBOL_BLANK and text)
+                or (symbol_set != self.SYMBOL_BLANK and not text)
+                or text not in symbol_set):
+            raise AssertionError(f'tile is not {name}: \'{tile.text}\'')
+
+
+# =========================== [ADD YOUR TESTS HERE] ===========================
+
+    def test_new_board_empty(self):
+        '''Check if a new game always starts with an empty board.'''
+        tiles = self.driver.find_elements(By.XPATH, self.BOARD_TILE_XPATH)
+        self.assertBoardEmpty(tiles)
+
+    def test_button_click(self):
+        '''Check if clicking the top-left button adds an X.'''
+        tiles = self.driver.find_elements(By.XPATH, self.BOARD_TILE_XPATH)
+        self.assertTileIs(tiles[0], self.SYMBOL_BLANK)
+        tiles[0].click()
+        self.assertTileIs(tiles[0], self.SYMBOL_X)
+
+
+# ================= [DO NOT MAKE ANY CHANGES BELOW THIS LINE] =================
+
+if __name__ != '__main__':
+    from selenium.webdriver import Firefox as Browser
+    from selenium.webdriver.common.by import By
+else:
+    parser = argparse.ArgumentParser(prog=sys.argv[0],
+                                     description='Chorus Lapilli Tester')
+    parser.add_argument('-b',
+                        '--browser',
+                        action='store',
+                        metavar='name',
+                        choices=['firefox', 'chrome', 'safari'],
+                        default='firefox',
+                        help='the browser to run tests with')
+    parser.add_argument('-c',
+                        '--change-dir',
+                        action='store',
+                        metavar='dir',
+                        default=None,
+                        help=('change the working directory before running '
+                              'tests'))
+
+    # Change the working directory
+    options = parser.parse_args(sys.argv[1:])
+    # Import different browser drivers based on user selection
+    try:
+        if options.browser == 'firefox':
+            from selenium.webdriver import Firefox as Browser
+        elif options.browser == 'chrome':
+            from selenium.webdriver import Chrome as Browser
+        else:
+            from selenium.webdriver import Safari as Browser
+        from selenium.webdriver.common.by import By
+    except ImportError as err:
+        print('[Error]',
+              err, '\n\n'
+              'Please refer to the Selenium documentation on installing the '
+              'webdriver:\n'
+              'https://www.selenium.dev/documentation/webdriver/'
+              'getting_started/',
+              file=sys.stderr)
+        sys.exit(1)
+
+    if options.change_dir:
+        try:
+            os.chdir(options.change_dir)
+        except OSError as err:
+            print(err, file=sys.stderr)
+            sys.exit(1)
+
+    if not os.path.isfile('package.json'):
+        print('Invalid directory: cannot find \'package.json\'',
+              file=sys.stderr)
+        sys.exit(1)
+
+    tests = unittest.defaultTestLoader.loadTestsFromTestCase(TestChorusLapilli)
+    unittest.TextTestRunner().run(tests)
